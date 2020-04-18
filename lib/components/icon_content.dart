3f93622d554349e7db8689f53dfd44371a2e7fb6
SHAH-JI <shahji1998@gmail.com>
2020-04-18 12:58:48 +0500

    Second Screen
    Designed the second screen that will show the results for BMI calculation

--- lib/components/icon_content.dart
+++ lib/components/icon_content.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'constants.dart';
+import '../constants.dart';
 
 class IconContents extends StatelessWidget {
   IconContents({this.icon, this.label});
